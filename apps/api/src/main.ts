c65945c7c6e08a71ac7ccdb2dc99d57f5922c479
Nguyen Minh Tuan <tnowad@gmail.com>
2025-03-27 08:16:58 +0700

    feat: add contracts package

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { apiContract } from '@fluentify/contracts';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT ?? 3200, () => {
+    logger.log(apiContract);
+    logger.log(`API is running on port ${process.env.PORT ?? 3200}`);
+  });
 }
 bootstrap();
