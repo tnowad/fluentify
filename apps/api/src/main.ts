3175c55ca482024f67e687a74959f227765dd5f7
Nguyen Minh Tuan <tnowad@gmail.com>
2025-03-28 09:11:42 +0700

    chore: remove app service and controller

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { apiContract } from '@workspace/contracts';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-
-  await app.listen(process.env.PORT ?? 3200, () => {
-    logger.log(apiContract);
-    logger.log(`API is running on port ${process.env.PORT ?? 3200}`);
-  });
+  await app.listen(process.env.PORT ?? 3200);
 }
-bootstrap();
+
+void bootstrap();
